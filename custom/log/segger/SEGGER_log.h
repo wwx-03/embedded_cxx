0766c3e0cdb313bc80bdff630b47078dd6b547b3
wwx-03 <2865096184@qq.com>
2026-02-02 08:41:59 +0800

    增加了LOGI_ARRY宏，用于打印数组

--- custom/log/segger/SEGGER_log.h
+++ custom/log/segger/SEGGER_log.h
@@ -13,7 +13,14 @@
 
 #define LOGI(format,...) SEGGER_RTT_printf(0, RTT_CTRL_TEXT_BRIGHT_GREEN format  RTT_CTRL_RESET, ##__VA_ARGS__)
 #define LOGW(format,...) SEGGER_RTT_printf(0, RTT_CTRL_TEXT_BRIGHT_YELLOW format  RTT_CTRL_RESET, ##__VA_ARGS__)
-#define LOGE(format,...) SEGGER_RTT_printf(0, RTT_CTRL_TEXT_BRIGHT_RED format  RTT_CTRL_RESET, ##__VA_ARGS__)
+
+#define LOGI_ARRAY(prefix, array, size) do {												\
+	LOGI(prefix ": ");																		\
+	for (size_t i = 0; i < size; ++i) {														\
+		SEGGER_RTT_printf(0, RTT_CTRL_TEXT_BRIGHT_GREEN "%02X " RTT_CTRL_RESET, array[i]);	\
+	}																						\
+	SEGGER_RTT_printf(0, "\r\n");															\
+} while (0)
 
 #else
 
@@ -21,6 +28,8 @@
 #define LOG(format,...)
 #define LOGI(format,...)
 #define LOGW(format,...)
-#define LOGE(format,...)
+#define LOGI_ARRAY(prefix, array, size)
 
 #endif
+
+#define LOGE(format,...) SEGGER_RTT_printf(0, RTT_CTRL_TEXT_BRIGHT_RED format  RTT_CTRL_RESET, ##__VA_ARGS__)
